6dbbede4b7f3599c77049b429d634983470ab69f
JagritiChopra <jagritichopra18@gmail.com>
2026-02-01 22:58:24 -0800

    fixed

--- backend/index.js
+++ backend/index.js
@@ -50,7 +50,9 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
+
+app.options(/.*/, cors(corsOptions));
+
 app.use(express.json());
 
 // ----------------- Prevent favicon/image crashes -----------------
